cbfcb10951457f6284a0f1911284bba121019c19
Josiah Teya <teyajosiah02@gmail.com>
2023-03-30 17:29:09 +0300

    commit assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const taskForm = document.getElementById("create-task-form");
+const taskList = document.getElementById("tasks");
+taskForm.addEventListener("submit", function(event) {
+  
+  event.preventDefault();
+
+  
+  const taskInput = document.getElementById("new-task-description");
+  const taskText = taskInput.value;
+
+  
+  const taskItem = document.createElement("li");
+  taskItem.innerText = taskText;
+
+  
+  taskList.appendChild(taskItem);
+
+
+  taskInput.value = "";
+});
+
 });
